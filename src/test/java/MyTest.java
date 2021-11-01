783052295f205d228192b73e091f491ca5c38151
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-11-01 20:31:56 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,33 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void testOddAndEven() {
+        assertEquals(true, OppositesAttract.isLove(1, 4));
+    }
 
+    @Test
+    public void testEvenAndEven() {
+        assertEquals(false, OppositesAttract.isLove(2, 2));
+    }
+
+    @Test
+    public void testOddAndOdd() {
+        assertEquals(false, OppositesAttract.isLove(1, 1));
+    }
+
+    @Test
+    public void testEvenAndOdd() {
+        assertEquals(true, OppositesAttract.isLove(0, 1));
+    }
+
+    private static class OppositesAttract {
+        public static boolean isLove(final int flower1, final int flower2) {
+       return flower1 % 2 != flower2 % 2;
+        }
     }
 }
+
+//return (flower1 == flower2) & false || (flower1 != flower2) & true;
